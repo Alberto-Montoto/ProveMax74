1ee163ad0d67485bbafa635b26422774cc7b9c7a
Alberto-Montoto <alberto_1422@hotmail.com>
2023-10-03 18:23:14 -0300

    Se crearon clases

--- src/provemax74/Entidades/Proveedor.java
+++ src/provemax74/Entidades/Proveedor.java
@@ -7,6 +7,53 @@ public class Proveedor {
     private String razonSocial;
 
     private String domicilio;
-
+    
     private String telefono;
+    
+    private Compra compra;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int idProveedor, String razonSocial, String domicilio, String telefono) {
+        this.idProveedor = idProveedor;
+        this.razonSocial = razonSocial;
+        this.domicilio = domicilio;
+        this.telefono = telefono;
+        this.compra = compra;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    
 }
