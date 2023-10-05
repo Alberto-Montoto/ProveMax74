bd50377d6a2b9ef783209db2a20bc500b19f646f
yirlen <yirlenstivencastanoaguirre@gmail.com>
2023-10-05 20:47:50 -0300

    des comente

--- src/provemax74/ProveMax74.java
+++ src/provemax74/ProveMax74.java
@@ -13,54 +13,43 @@ public class ProveMax74 {
         // TODO code application logic here
 
         Connection con = Conexion.getConexion();
-        
-        //////metodo agregarProducto//////
-//                                   idpro/nombrePro/descripcion/precioAct/Stok/estado
 
+        //////metodo agregarProducto//////
+//                                  idpro/nombreP/descrip/precioAct/Stok/estado
         Producto arroz = new Producto(111, "azucar", "morena", 123, 50, true);
         ProductoData pro = new ProductoData(con);
 
 //        Producto arroz = new Producto(333, "azucar", "morena", 123, 50, true);
 //        ProductoData pro = new ProductoData(con);
-
-
 //        pro.agregarProducto(arroz);
-        
+
         //////metodo eliminarProducto////// 
-              //  pro.eliminarProducto(333);
+        //  pro.eliminarProducto(333);
         
         //////metodo modificarProducto////// 
-
-             //   pro.modificarProducto(arroz);
-
-//              pro.modificarProducto(arroz);
-
-
+        //   pro.modificarProducto(arroz);
+        
         //////metodo obtenerProducto////// 
 //              Producto obtenerProducto =pro.obtenerProducto(111);
 //               if (obtenerProducto!=null) {    
 //        System.out.println("nombreProducto " + obtenerProducto.getNombreProducto());
 //        System.out.println("descripcion "+ obtenerProducto.getDescripcion());
 //        }
-             
+
         //////metodo listarProductos////// 
-                 for (Producto producto: pro.listarProducotos()) {
-            System.out.println("idProducto: " + producto.getIdProducto());
-            System.out.println("nombreProducto: " + producto.getNombreProducto());
-            System.out.println("descripcion: " + producto.getDescripcion());
-            System.out.println("precioActual: " + producto.getPrecioActual());
-            System.out.println("stock: " + producto.getStock());
-            System.out.println("------------------------------");
-        }  
-                 
-                 //        for (Alumno alumno: alu.listarAlumnos()) {
-//            System.out.println("DNI: " + alumno.getDni());
-//            System.out.println("Apellido: " + alumno.getApellido());
-//            System.out.println("Nombre: " + alumno.getNombre());
-//            System.out.println("Fecha de Nacimiento: " + alumno.getFechaNacimiento());
+//                 for (Producto producto: pro.listarProducotos()) {
+//            System.out.println("idProducto: " + producto.getIdProducto());
+//            System.out.println("nombreProducto: " + producto.getNombreProducto());
+//            System.out.println("descripcion: " + producto.getDescripcion());
+//            System.out.println("precioActual: " + producto.getPrecioActual());
+//            System.out.println("stock: " + producto.getStock());
 //            System.out.println("------------------------------");
-//        }
-              
+//        }  
+
+        ////////////////// Hasta aqui Metodos de productoData ////////////////
+        
+      
+        
 //          Proveedor prove=new Proveedor(1,"Wachoski","Electro Mayor","Juan B Justo","011123456");
 //          ProveedorData proveData=new ProveedorData();
 //          
@@ -73,4 +62,3 @@ public class ProveMax74 {
 //              
     }
 }
-
