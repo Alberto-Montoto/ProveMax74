752f349613fef2bacd8bb93f9f3ccf92bc3f7a51
lucia-barbosa <lucia.ib100@gmail.com>
2023-10-18 19:42:11 -0300

    Actualizacion de la base de datos Provemax74

--- Base de datos/provemax74.sql
+++ Base de datos/provemax74.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-10-2023 a las 00:56:23
+-- Tiempo de generación: 19-10-2023 a las 00:41:14
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -32,7 +32,6 @@ USE `provemax74`;
 CREATE TABLE `compra` (
   `idCompra` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `idDetalle` int(11) NOT NULL,
   `idProveedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -40,9 +39,12 @@ CREATE TABLE `compra` (
 -- Volcado de datos para la tabla `compra`
 --
 
-INSERT INTO `compra` (`idCompra`, `fecha`, `idDetalle`, `idProveedor`) VALUES
-(1, '2023-10-11', 501, 10),
-(2, '2023-10-11', 501, 7);
+INSERT INTO `compra` (`idCompra`, `fecha`, `idProveedor`) VALUES
+(1, '2023-10-18', 5),
+(2, '2023-10-18', 6),
+(3, '2023-10-17', 7),
+(4, '2023-10-16', 8),
+(5, '2023-10-15', 5);
 
 -- --------------------------------------------------------
 
@@ -54,19 +56,17 @@ CREATE TABLE `detallecompra` (
   `idDetalle` int(11) NOT NULL,
   `cantidad` int(11) NOT NULL,
   `precioCosto` double NOT NULL,
-  `nombreProducto` int(50) NOT NULL,
-  `idProducto` int(11) NOT NULL
+  `idProducto` int(11) NOT NULL,
+  `idCompra` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `detallecompra`
 --
 
-INSERT INTO `detallecompra` (`idDetalle`, `cantidad`, `precioCosto`, `nombreProducto`, `idProducto`) VALUES
-(500, 500, 0, 0, 111),
-(501, 60, 600, 0, 111),
-(502, 60, 600, 0, 111),
-(503, 60, 600, 0, 111);
+INSERT INTO `detallecompra` (`idDetalle`, `cantidad`, `precioCosto`, `idProducto`, `idCompra`) VALUES
+(1, 10, 2000, 334, 1),
+(2, 10, 3000, 338, 2);
 
 -- --------------------------------------------------------
 
@@ -88,9 +88,16 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`idProducto`, `nombreProducto`, `descripcion`, `precioActual`, `stock`, `estado`) VALUES
-(111, 'arroz', 'fino', 500, 150, 1),
-(222, 'fideos', 'tirabuzon', 200, 100, 1),
-(333, 'azucar', 'morena', 123, 50, 1);
+(334, 'Lavarropas', 'Drean', 200, 50, 1),
+(335, 'Lavarropas', 'Whirpool', 300, 50, 1),
+(336, 'Lavavajillas', 'Samsung', 500, 50, 1),
+(337, 'Lavavajillas', 'LG', 400, 50, 1),
+(338, 'Heladera', 'Conqueror', 300, 50, 1),
+(339, 'Heladera', 'Gafa', 400, 50, 1),
+(340, 'Cocina', 'Domec', 300, 50, 1),
+(341, 'Cocina', 'Florencia', 200, 50, 1),
+(342, 'Microondas', 'Philips', 100, 50, 1),
+(343, 'Microondas', 'Atma', 150, 50, 1);
 
 -- --------------------------------------------------------
 
@@ -100,7 +107,6 @@ INSERT INTO `producto` (`idProducto`, `nombreProducto`, `descripcion`, `precioAc
 
 CREATE TABLE `proveedor` (
   `idProveedor` int(11) NOT NULL,
-  `nombreCompleto` varchar(50) NOT NULL,
   `razonSocial` varchar(50) NOT NULL,
   `domicilio` varchar(50) NOT NULL,
   `telefono` varchar(20) NOT NULL
@@ -110,11 +116,13 @@ CREATE TABLE `proveedor` (
 -- Volcado de datos para la tabla `proveedor`
 --
 
-INSERT INTO `proveedor` (`idProveedor`, `nombreCompleto`, `razonSocial`, `domicilio`, `telefono`) VALUES
-(7, 'Polanski', 'Ventas Electros', 'Caseros 123', '011888555'),
-(8, 'Conti', 'Todo Electro', 'San Martin 4567', '011777666'),
-(9, 'Sarmiento', 'Electro Max', 'Belgrano 4567', '011222333'),
-(10, 'Paez', 'Electrodomesticos Power', 'Belgrano 4567', '011222333');
+INSERT INTO `proveedor` (`idProveedor`, `razonSocial`, `domicilio`, `telefono`) VALUES
+(5, 'Musimundo', 'Carabobo 789', '011123456'),
+(6, 'Red Electro', 'Florida 1415', '011181920'),
+(7, 'Ventas Electros', 'Caseros 123', '011888555'),
+(8, 'Todo Electro', 'San Martin 4567', '011777666'),
+(9, 'Electro Max', 'Rivadavia 4567', '011212223'),
+(10, 'Electrodomesticos Power', 'Belgrano 4567', '011222333');
 
 --
 -- Índices para tablas volcadas
@@ -125,7 +133,6 @@ INSERT INTO `proveedor` (`idProveedor`, `nombreCompleto`, `razonSocial`, `domici
 --
 ALTER TABLE `compra`
   ADD PRIMARY KEY (`idCompra`),
-  ADD KEY `idDetalle` (`idDetalle`),
   ADD KEY `idProveedor` (`idProveedor`);
 
 --
@@ -133,7 +140,8 @@ ALTER TABLE `compra`
 --
 ALTER TABLE `detallecompra`
   ADD PRIMARY KEY (`idDetalle`),
-  ADD KEY `idProducto` (`idProducto`);
+  ADD KEY `idProducto` (`idProducto`),
+  ADD KEY `idCompra` (`idCompra`);
 
 --
 -- Indices de la tabla `producto`
@@ -155,7 +163,7 @@ ALTER TABLE `proveedor`
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `idCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `detallecompra`
@@ -167,7 +175,7 @@ ALTER TABLE `detallecompra`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=334;
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=344;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
@@ -183,14 +191,14 @@ ALTER TABLE `proveedor`
 -- Filtros para la tabla `compra`
 --
 ALTER TABLE `compra`
-  ADD CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idDetalle`) REFERENCES `detallecompra` (`idDetalle`),
   ADD CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`idProveedor`) REFERENCES `proveedor` (`idProveedor`);
 
 --
 -- Filtros para la tabla `detallecompra`
 --
 ALTER TABLE `detallecompra`
-  ADD CONSTRAINT `detallecompra_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`);
+  ADD CONSTRAINT `detallecompra_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
+  ADD CONSTRAINT `detallecompra_ibfk_2` FOREIGN KEY (`idCompra`) REFERENCES `compra` (`idCompra`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
